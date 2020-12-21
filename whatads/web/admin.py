f18d7c0e66ab49849b2a9cf165fc637cfef48cbe
admin <almajanltd@gmail.com>
2020-12-21 02:14:40 +0000

    messages arrival check

--- whatads/web/admin.py
+++ whatads/web/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import SendMsg, SendImg, SendVdo, SendVce
+from .models import SendMsg, SendImg, SendVdo, SendVce, CheckDlvy, CheckSeen
 
 # Register your models here.
 admin.site.register(SendMsg)
 admin.site.register(SendImg)
 admin.site.register(SendVdo)
 admin.site.register(SendVce)
+admin.site.register(CheckDlvy)
+admin.site.register(CheckSeen)
--- whatads/web/models.py
+++ whatads/web/models.py
@@ -4,12 +4,31 @@ from django.db import models
 
 class SendMsg(models.Model):
         text = models.CharField(max_length=255)
+        def __unicode__(self):
+            return self.text
 
 class SendImg(models.Model):
         text = models.CharField(max_length=255)
+        def __unicode__(self):
+            return self.text
 
 class SendVdo(models.Model):
         text = models.CharField(max_length=255)
+        def __unicode__(self):
+            return self.text
 
 class SendVce(models.Model):
         text = models.CharField(max_length=255)
+        def __unicode__(self):
+            return self.text
+
+
+class CheckDlvy(models.Model):
+        text = models.CharField(max_length=255)
+        def __unicode__(self):
+            return self.text
+
+class CheckSeen(models.Model):
+        text = models.CharField(max_length=255)
+        def __unicode__(self):
+            return self.text
